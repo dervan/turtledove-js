05ac24a9d597964d0d80f9a423e6e529d497be2b
Michał Jagielski <michal@jagielski.net>
2020-09-09 00:15:45 +0200

    Fetch ads can return multiple ads

--- ad-network/ad-network.js
+++ ad-network/ad-network.js
@@ -1,30 +1,26 @@
+import { generateAdNetworkAppAsync, InterestGroupAd, ContextualBidResponse } from 'turtledove-js-api'
 import express from 'express'
-import cors from 'cors'
-import fs from 'fs'
-import bodyParser from 'body-parser'
-import path from 'path'
-import { InterestGroupAd, ContextualBidResponse } from 'turtledove-js-api'
 
-import { ports, addresses } from '../config.js'
+import fs from 'fs'
+import path from 'path'
+
 import { computeInterestGroupSignals, interestGroupToAdParams } from './interest-group-evaluation.js'
 import { extractContextSignals, getContextualAd } from './context-evaluation.js'
+import { ports, addresses } from '../config.js'
 
 const __dirname = path.resolve('./ad-network')
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
 
 /**
- * For given InterestGroup returns prepared InterestGroupAd, in the format described by TURTLEDOVE.
+ * For given InterestGroup returns list of prepared InterestGroupAds, in the format described by TURTLEDOVE.
  * @param {string} interestGroupKey
- * @returns {Promise<InterestGroupAd>}
+ * @returns {Promise<InterestGroupAd[]>}
  */
 async function fetchAds (interestGroupKey) {
   console.log(`Fetch ads for: ${interestGroupKey}`)
   const adParams = interestGroupToAdParams(interestGroupKey)
   const igSignals = computeInterestGroupSignals(interestGroupKey)
   const bidFunctionUrl = addresses.adPartner + '/static/bidding-function.js'
-  return new InterestGroupAd(interestGroupKey, await adParams.generateAdHtml(), igSignals, bidFunctionUrl, addresses.adPartner)
+  return [new InterestGroupAd(interestGroupKey, await adParams.generateAdHtml(), igSignals, bidFunctionUrl, addresses.adPartner)]
 }
 
 /**
@@ -40,11 +36,7 @@ async function fetchContextualBid (contextualBidRequest) {
   return new ContextualBidResponse(contextSignals, evaluatedContextualAd?.contextualAd, evaluatedContextualAd?.bidValue)
 }
 
-app.get('/fetch-ads', async (req, res) => {
-  res.json(await fetchAds(decodeURIComponent(req.query.interest_group)))
-})
-app.post('/fetch-context-bid', async (req, res) => res.json(await fetchContextualBid(req.body)))
-app.post('/fetch-contextual-bid', async (req, res) => res.json(await fetchContextualBid(req.body)))
+const app = generateAdNetworkAppAsync(fetchAds, fetchContextualBid)
 app.use('/static', express.static(`${__dirname}/content/static`))
 app.get('/', (req, res) => {
   res.set('Content-Type', 'text/html')
--- turtledove-server/content/static/js/store.js
+++ turtledove-server/content/static/js/store.js
@@ -92,11 +92,52 @@ function removeAds (interestGroup, logger) {
 }
 
 /**
+ * Returns an function that will download bidding function source code from a specified address
+ * and put it inside an Ad for every ad on a list.
+ *
+ * @param {string} adKey
+ * @param {string} readerAdsKey
+ * @param {Logger} logger
+ */
+function enrichAdsWithBiddingFunctions (logger) {
+  return interestBasedAds => Promise.allSettled(
+    interestBasedAds.map(ad => fetch(ad.bidFunctionSrc)
+      .then(async (scriptResponse) => {
+        if (!scriptResponse.ok) {
+          logger.log(`Request for bid function of ad ${ad.id} returned ${scriptResponse.status}`)
+          return null
+        }
+        ad.bidFunction = await scriptResponse.text()
+        return ad
+      }).catch(() => logger.log(`Cannot download bid function for ${ad.id}`))))
+}
+
+/**
+ * Returns an function that will save list of ads into localStorage
+ *
+ * @param {string} adKey
+ * @param {string} readerAdsKey
+ * @param {Logger} logger
+ */
+function saveAds (adKey, readerAdsKey, logger) {
+  return interestBasedAds => {
+    for (const ad of interestBasedAds) {
+      if (ad !== null && ad.status === 'fulfilled' && ad.bidFunctionSrc !== null) {
+        const localAds = JSON.parse(window.localStorage.getItem(readerAdsKey)) || {}
+        localAds[adKey] = ad.value
+        logger.log(`Fetched new ad for group ${adKey}.`)
+        window.localStorage.setItem(readerAdsKey, JSON.stringify(localAds))
+      }
+    }
+  }
+}
+
+/**
  * Performs a call to fetch-ads. Due to demo simplicity, it is performed always after adding to a user group
  * (on the contrary to standard TURTLEDOVE, where this call will be asynchronous)
  *
  * @param {InterestGroup} interestGroup
- * @param logger
+ * @param {Logger} logger
  */
 function fetchNewAds (interestGroup, logger) {
   const timeout = 1000
@@ -104,22 +145,12 @@ function fetchNewAds (interestGroup, logger) {
 
   for (let i = 0; i < interestGroup.readers.length; i++) {
     const reader = interestGroup.readers[i]
-    const readerAdsKey = fetchedAdsStorageKeyPrefix + reader
     const controller = new AbortController()
     fetch(`${reader}/fetch-ads?interest_group=${encodeURIComponent(adKey)}`, { signal: controller.signal })
-      .then((r) => r.json())
-      .then(interestBasedAdResponse => {
-        return fetch(interestBasedAdResponse.bidFunctionSrc)
-          .then(async (scriptResponse) => {
-            interestBasedAdResponse.bidFunction = await scriptResponse.text()
-            return interestBasedAdResponse
-          })
-      }).then(interestBasedAd => {
-        const localAds = JSON.parse(window.localStorage.getItem(readerAdsKey)) || {}
-        localAds[adKey] = interestBasedAd
-        logger.log(`Fetched new ad for group ${adKey}.`)
-        window.localStorage.setItem(readerAdsKey, JSON.stringify(localAds))
-      }).catch(reason => logger.log(`Request to ${reader} for ${adKey} failed: ${reason}`))
+      .then(r => r.json())
+      .then(enrichAdsWithBiddingFunctions(logger))
+      .then(saveAds(adKey, fetchedAdsStorageKeyPrefix + reader, logger))
+      .catch(reason => logger.log(`Request to ${reader} for ${adKey} failed: ${reason}`))
     setTimeout(() => controller.abort(), timeout)
   }
 }
